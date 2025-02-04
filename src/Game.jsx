f4e8913707d688d50c0b215b462c76138dd897d6
Ahmed Ahmed <103862119+AhmedDoesCS@users.noreply.github.com>
2025-02-04 14:04:37 +0200

    Test

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 function Game() {
 
-    window.SpeechRecognition = window.SpeechRecognition
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new window.SpeechRecognition();
+
+    recognition.interimResults = true;
+
+    let p = document.createElement("p");
+    recognition.addEventListener('result', (e) => {
+        console.log(e.results);
+    })
+
+    recognition.start();
 
   return <>
+  <div>
+
+  </div>
     <div className='w-[100%] h-[100vh] py-[10%] px-[6%] bg-cyan-100 '>
         <div className='w-[100%] h-[100%] [word-spacing:20px] text-wrap text-[35px]/15 font-[600] text-black/40 flex justify-center overflow-scroll'>
         The ball is red.  It is a big red ball.
