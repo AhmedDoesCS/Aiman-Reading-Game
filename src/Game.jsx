6f905f07f1ca30f752f95d056ab9c3b912f7c3ef
Ahmed Ahmed <103862119+AhmedDoesCS@users.noreply.github.com>
2025-02-05 10:03:11 +0200

    Speech Recognition test

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,8 +4,8 @@ function Game() {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     recognition.lang = 'en';
-    recognition.interimResults = 'false';
-    recognition.continuous = 'true';
+    recognition.interimResults = false;
+    recognition.continuous = true;
 
     function startButton() {
         const textContent = document.getElementById("textContent");
@@ -17,7 +17,6 @@ function Game() {
     function stopButton() {
         const textContent = document.getElementById("textContent");
         recognition.stop();
-
         console.log("Recognition stopped");
     }
 
@@ -31,7 +30,7 @@ function Game() {
     recognition.onresult = (e) => {
         console.log(e);
         const textContent = document.getElementById("textContent");
-        textContent.innerText += e.results[e.results.length - 1][0].transcript;
+        textContent.textContent += e.results[e.results.length - 1][0].transcript + " ";
     }
 
     return <>
